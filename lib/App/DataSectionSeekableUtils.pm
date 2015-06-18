b7e28978d08145e97d25f233c2eca766eb8f2791
perlancar (@pc-office) <perlancar@gmail.com>
2015-06-18 15:05:51 +0700

    Initial commit

---
+++ lib/App/DataSectionSeekableUtils.pm
@@ -0,0 +1,23 @@
+package App::DataSectionSeekableUtils;
+
+# DATE
+# VERSION
+
+use 5.010001;
+
+1;
+# ABSTRACT: Collection of CLI utilities related to Data::Section::Seekable
+
+=head1 DESCRIPTION
+
+This distribution provides the following command-line utilities related to
+L<Data::Section::Seekable>:
+
+#INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<Data::Section::Seekable>
+
+=cut
